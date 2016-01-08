651f5a92d7ae1a5730e6e6dc51face6baa6c7081
Korvinius <korvin.wma@gmail.com>
2016-01-08 14:21:21 +0200

    v1.3

--- src/net/wealth_mc/nomult/NoMult.java
+++ src/net/wealth_mc/nomult/NoMult.java
@@ -1,8 +1,10 @@
 package net.wealth_mc.nomult;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import net.wealth_mc.nomult.other.NoMultVault;
 import net.wealth_mc.nomult.yaml.YamlGroup;
@@ -10,6 +12,7 @@ import net.wealth_mc.nomult.yaml.YamlMult;
 import net.wealth_mc.nomult.yaml.YamlNotMult;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 
@@ -21,6 +24,7 @@ public class NoMult extends JavaPlugin {
 	public Map<String, String> mult = new HashMap<String, String>();
 	public Map<String, String> groups = new HashMap<String, String>();
 	public List<String> notmult;
+	public static Set<Player> playerlogin = new HashSet<Player>();
 	
 	private NoMultCmd nomultcmd;
 	private YamlMult yamlmult = new YamlMult();
@@ -31,7 +35,6 @@ public class NoMult extends JavaPlugin {
 	public String ngroup;
 	public String defgroup;
 	public long times;
-	public static boolean pjoin;
 	public static boolean blockjoin;
 	public static boolean blockleave;
 	public static String plogin;
@@ -43,7 +46,7 @@ public class NoMult extends JavaPlugin {
 	@Override
 	public void onEnable() {	
 		instance = this;		
-		config = this.getConfig();		
+		config = this.getConfig();
 		this.getConfig().options().copyDefaults(true).copyHeader(true);
 		this.saveDefaultConfig();
 
@@ -52,7 +55,6 @@ public class NoMult extends JavaPlugin {
 		defgroup = config.getString("default");
 		times = config.getLong("times");
 		plogin = config.getString("message");
-		pjoin  = config.getBoolean("login");
 		plogout = config.getString("message2");
 		plmult = config.getString("message3");
 		plreg = config.getString("message4");
@@ -95,17 +97,17 @@ public class NoMult extends JavaPlugin {
 	
 	public void reloadConf() {
 		this.reloadConfig();
-		debug = this.getConfig().getBoolean("debug");
-		ngroup = this.getConfig().getString("group");
-		defgroup = this.getConfig().getString("default");
-		plogin = this.getConfig().getString("message");
-		pjoin  = this.getConfig().getBoolean("login");
-		plogout = this.getConfig().getString("message2");
+		config = this.getConfig();
+		debug = config.getBoolean("debug");
+		ngroup = config.getString("group");
+		defgroup = config.getString("default");
+		plogin = config.getString("message");
+		plogout = config.getString("message2");
 		plmult = config.getString("message3");
 		plreg = config.getString("message4");
-		blockjoin  = this.getConfig().getBoolean("blockjoin");
-		blockleave  = this.getConfig().getBoolean("blockleave");
-		pvp  = this.getConfig().getBoolean("pvp");
+		blockjoin  = config.getBoolean("blockjoin");
+		blockleave  = config.getBoolean("blockleave");
+		pvp  = config.getBoolean("pvp");
 	}
 	
 	public void saveConf() {
--- src/net/wealth_mc/nomult/NoMultList.java
+++ src/net/wealth_mc/nomult/NoMultList.java
@@ -2,6 +2,7 @@ package net.wealth_mc.nomult;
 
 import java.net.InetAddress;
 
+import net.wealth_mc.nomult.other.NoMultLogin;
 import net.wealth_mc.nomult.other.NoMultRun;
 
 import org.bukkit.ChatColor;
@@ -15,7 +16,6 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 import fr.xephi.authme.events.LoginEvent;
-import fr.xephi.authme.events.LogoutEvent;
 
 public class NoMultList implements Listener {
 
@@ -31,37 +31,24 @@ public class NoMultList implements Listener {
 
 	@EventHandler
 	   public void onQuitPlayer(PlayerQuitEvent event){
-		if (NoMult.blockleave) event.setQuitMessage(null);
+		if (NoMult.blockleave) {
+			event.setQuitMessage(null);
+			new NoMultLogin(event.getPlayer(), false);
+		}
 	}
 
 	@EventHandler
 	public void onAuthLoginEvent(LoginEvent event) {
-		if (NoMult.pjoin) checkAuthLoginEvent(event);
-	}
-
-	@EventHandler
-	public void onAuthLogoutEvent(LogoutEvent event) {
-		checkAuthLogoutEvent(event);
+		Player player = event.getPlayer();
+		new NoMultRun(player);
+		if (NoMult.blockjoin) {
+			new NoMultLogin(event.getPlayer(), true);
+		}
 	}
 
 	@EventHandler(priority = EventPriority.LOW)
 	public void onEntityDamage(EntityDamageEvent event) {
 		checkEntityDamage(event);
-	}
-
-	private void checkAuthLogoutEvent(LogoutEvent event) {
-		if (!event.getPlayer().hasPermission("nomult.spy")) {
-			NoMult.instance.getServer().broadcastMessage(ChatColor.GOLD + event.getPlayer().getName() 
-					+ ChatColor.YELLOW + NoMult.plogout);
-		}
-	}
-	private void checkAuthLoginEvent(LoginEvent event) {
-		Player player = event.getPlayer();
-		if (!event.getPlayer().hasPermission("nomult.spy")) {
-			NoMult.instance.getServer().broadcastMessage(ChatColor.GOLD + event.getPlayer().getName() 
-					+ ChatColor.YELLOW + NoMult.plogin);
-		}
-		new NoMultRun(player);
 	}
 
 	private void checkEntityDamage(EntityDamageEvent event) {
@@ -74,14 +61,13 @@ public class NoMultList implements Listener {
 			InetAddress tak = null;
 			
 			if (event instanceof EntityDamageByEntityEvent) {
-				EntityDamageByEntityEvent edbe = (EntityDamageByEntityEvent) event;
-				EntityDamageByEntityEvent edbi = (EntityDamageByEntityEvent) event;
+				EntityDamageByEntityEvent entity = (EntityDamageByEntityEvent) event;
 				
-				if ((edbe.getDamager() instanceof Player)
-						&& (edbi.getEntity() instanceof Player)) {
-					damager = (Player) edbe.getDamager();
-					taker = (Player) edbi.getEntity();
-					if (edbi.getDamager() != null && taker.isOnline()) {
+				if ((entity.getDamager() instanceof Player)
+						&& (entity.getEntity() instanceof Player)) {
+					damager = (Player) entity.getDamager();
+					taker = (Player) entity.getEntity();
+					if (entity.getDamager() != null && taker.isOnline()) {
 						dam = damager.getAddress().getAddress();
 						tak = taker.getAddress().getAddress();
 						if (dam.equals(tak)){
